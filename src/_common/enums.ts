c574319704310c54ca200484d3143317c8b8ddad
NadirBakhsh <nadir4k2010@gmail.com>
2025-08-12 00:24:46 +0500

    enrollment injecting in service

---
+++ src/_common/enums.ts
@@ -0,0 +1,16 @@
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  STAFF = 'STAFF',
+  STUDENT = 'STUDENT',
+}
+
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+}
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  OTHER = 'OTHER',
+}
--- src/enrollment/enrollment.service.ts
+++ src/enrollment/enrollment.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateEnrollmentDto } from './dto/create-enrollment.dto';
 import { UpdateEnrollmentDto } from './dto/update-enrollment.dto';
+import { Enrollment } from './enrollment.entity';
 
 @Injectable()
 export class EnrollmentService {
+  constructor(
+    @InjectRepository(Enrollment)
+    private readonly enrollmentRepository: Repository<Enrollment>,
+  ) {}
   create(createEnrollmentDto: CreateEnrollmentDto) {
     return 'This action adds a new enrollment';
   }
